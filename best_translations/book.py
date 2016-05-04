58be54e54f6d8a339c1e40ca47d3c30b9e475d8a
ovallej <ovallej@seas.upenn.edu>
2016-05-04 18:00:40 -0400

    comments

--- best_translations/book.py
+++ best_translations/book.py
@@ -20,6 +20,8 @@ def make_book(file) :
 		if len(matches) > 0:
 			page = reg_num.findall(matches[0])[0]
 			curr_key = matches[0]
+			''' all this stuff is to ensure
+			pages are numbered and ordered correctly '''
 			page_char = str(unichr(int(page) + 65))
 			curr_key = curr_key.replace(page, page_char)
 			old = reg_link.findall(i)[0]
@@ -29,6 +31,7 @@ def make_book(file) :
 	file_object.close()
 	li = d.keys()
 	li.sort()
+	'''sorts the pages for us'''
 	breaks = [0]
 	books = []
 	curr_name = li[0][0:4]
@@ -39,12 +42,13 @@ def make_book(file) :
 	books = ["" for i in breaks]
 	curr_name = li[0][0:4]
 	counter = 0
+	''' adds each page to a string for the respective book '''
 	for x in li:
 		if curr_name not in x:
 			curr_name = x[0:4]
 			counter += 1
 		books[counter] = books[counter] + d[x]
-	
+	''' writes the book'''
 	for i,book in enumerate(books):
 		title = li[breaks[i]]
 		direct = 'student_text_files'
